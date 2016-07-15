a202f9099a6b74cbfcfc4a86c1ee3158cc87d45f
A1exandra <parhimchik1995@mail.ru>
2016-07-15 21:37:44 +0300

    without pixelperfect

---
+++ task1/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+http_path = '/'             #root path
+relative_assets = true      #because we're not working from the root
+css_dir = "css"             #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+images_dir = "img"          #the folder with your images
+javascripts_dir = "js"      #the folder with your javascript
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
