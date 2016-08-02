9ea5ab63fb0eeb0090299df1acb672fc9e785aeb
a.parkhimchyk <parhimchik1995@mail.ru>
2016-08-02 19:05:51 +0300

    module pattern

--- jstasks/ajax/js/main.js
+++ jstasks/ajax/js/main.js
@@ -1,61 +1,111 @@
-$('#weather').click(function () {
-    $.ajax({
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22minsk%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-        success: function (json_weather) {
+var myModule = (function (opts) {
 
-            // console.log(json_weather);
-            $('<h2>').text(json_weather.query.results.channel.title).appendTo('.weather');
-            $('<h3>').text('Date: ').appendTo('.weather');
-            $('.weather').append(json_weather.query.results.channel.item.condition.date);
-            $('<h3>').text('Temperature: ').appendTo('.weather');
-            $('.weather').append(json_weather.query.results.channel.item.condition.temp);
-        }
+    if (opts.weather && opts.finance) {
+
+        /*Q.fcall($.ajax({
+            url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22minsk%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+            success: function (json_weather) {
+
+                $('<h2>').text(json_weather.query.results.channel.title).appendTo('.' + opts.type);
+                $('<h3>').text('Date: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.date);
+                $('<h3>').text('Temperature: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.temp);
+
+                $.ajax({
+                    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+                    success: function (json_finance) {
+
+                        $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.' + opts.type);
+                        $('<h3>').text('Exchange rates: ').appendTo('.' + opts.type);
+                        $('.' + opts.type).append(json_finance.query.results.quote[0].LastTradePriceOnly);
+                    }
+                });
+            }
+        }))
+            .then($.ajax({
+                url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+                success: function (json_finance) {
+
+                    $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.' + opts.type);
+                    $('<h3>').text('Exchange rates: ').appendTo('.' + opts.type);
+                    $('.' + opts.type).append(json_finance.query.results.quote[0].LastTradePriceOnly);
+                }
+            }))
+            .catch(function (error) {
+                // Handle any error from all above steps
+            })
+            .done();*/
+        $.ajax({
+            url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22minsk%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+            success: function (json_weather) {
+
+                $('<h2>').text(json_weather.query.results.channel.title).appendTo('.' + opts.type);
+                $('<h3>').text('Date: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.date);
+                $('<h3>').text('Temperature: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.temp);
+
+                $.ajax({
+                    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+                    success: function (json_finance) {
+
+                        $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.' + opts.type);
+                        $('<h3>').text('Exchange rates: ').appendTo('.' + opts.type);
+                        $('.' + opts.type).append(json_finance.query.results.quote[0].LastTradePriceOnly);
+                    }
+                });
+            }
+        });
+    } else if (opts.weather) {
+
+        $.ajax({
+            url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22minsk%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+            success: function (json_weather) {
+
+                $('<h2>').text(json_weather.query.results.channel.title).appendTo('.' + opts.type);
+                $('<h3>').text('Date: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.date);
+                $('<h3>').text('Temperature: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_weather.query.results.channel.item.condition.temp);
+            }
+        });
+    } else if (opts.finance) {
+
+        $.ajax({
+            url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+            success: function (json_finance) {
+
+                $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.' + opts.type);
+                $('<h3>').text('Exchange rates: ').appendTo('.' + opts.type);
+                $('.' + opts.type).append(json_finance.query.results.quote[0].LastTradePriceOnly);
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+
+    $('#weather').click(function () {
+        myModule({
+            type: 'weather',
+            weather: true
+        });
+    });
+
+    $('#finance').click(function () {
+        myModule({
+            type: 'finance',
+            finance: true
+        });
+    });
+
+    $('#total').click(function () {
+        myModule({
+            type: 'total',
+            weather: true,
+            finance: true
+        });
     });
 });
 
-/*minsk-834463
-milan-718345
-new-york-2459115
-london-44418
-paris-615702
-chicago-2379574*/
-
-$('#finance').click(function () {
-    $.ajax({
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
-        success: function (json_finance) {
-
-            // console.log(json_finance);
-            $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.finance');
-            // $('<h3>').text('Date: ').appendTo('.finance');
-            $('<h3>').text('Exchange rates: ').appendTo('.finance');
-            $('.finance').append(json_finance.query.results.quote[0].LastTradePriceOnly);
-        }
-    });
-});
-
-$('#total').click(function () {
-
-    $.ajax({
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22minsk%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
-        success: function (json_weather) {
-
-            $('<h2>').text(json_weather.query.results.channel.title).appendTo('.total');
-            $('<h3>').text('Date: ').appendTo('.total');
-            $('.total').append(json_weather.query.results.channel.item.condition.date);
-            $('<h3>').text('Temperature: ').appendTo('.total');
-            $('.total').append(json_weather.query.results.channel.item.condition.temp);
-        }
-    });
-
-    $.ajax({
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22USD%22%2C%22BYN%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
-        success: function (json_finance) {
-            
-            $('<h2>').text('Yahoo! Finance - Minsk, Minsk, BY').appendTo('.total');
-            // $('<h3>').text('Date: ').appendTo('.finance');
-            $('<h3>').text('Exchange rates: ').appendTo('.total');
-            $('.total').append(json_finance.query.results.quote[0].LastTradePriceOnly);
-        }
-    });
-});
