cba6ad19e550ff1a076d3d78e302b5461df531bb
a.parkhimchyk <parhimchik1995@mail.ru>
2016-08-08 18:15:48 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-var gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssmin = require('gulp-cssmin'),
-    hint = require('gulp-htmlhint'),
-    min = require('gulp-htmlmin'),
-    imagemin = require('gulp-imagemin'),
-    rigger = require('gulp-rigger'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'),
-    watch = require('gulp-watch'),
-    pngquant = require('imagemin-pngquant');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var cache_bust = require('gulp-cache-bust');
+var csscomb = require('gulp-csscomb');
+var csslint = require('gulp-csslint');
+var cssmin = require('gulp-cssmin');
+var csso = require('gulp-csso');
+var fixmyjs = require('gulp-fixmyjs');
+var htmlhint = require('gulp-htmlhint');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var jshint = require('gulp-jshint');
+var jsonlint = require('gulp-jsonlint');
+var rigger = require('gulp-rigger');
+var sass = require('gulp-sass');
+var spritesmith = require('gulp-spritesmith');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
 
 var path = {
     build: {
@@ -37,42 +45,69 @@ var path = {
 gulp.task('html:build', function () {
     gulp.src(path.src.html)
         .pipe(rigger())
-        .pipe(hint())
-        .pipe(min())
+        .pipe(htmlhint())
+        .pipe(htmlmin())
+        .pipe(cache_bust())
         .pipe(gulp.dest(path.build.html));
-});
-
-gulp.task('js:build', function () {
-    gulp.src(path.src.js)
-        .pipe(rigger())
-        .pipe(uglify())
-        .pipe(gulp.dest(path.build.js));
 });
 
 gulp.task('css:build', function () {
     gulp.src(path.src.css)
         .pipe(sass())
         .pipe(autoprefixer())
+        .pipe(csscomb())
+        .pipe(csslint())
         .pipe(cssmin())
+        .pipe(csso())
+        .pipe(cache_bust())
         .pipe(gulp.dest(path.build.css));
 });
 
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(jshint())
+        .pipe(fixmyjs())
+        .pipe(uglify())
+        .pipe(cache_bust())
+        .pipe(gulp.dest(path.build.js));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            styleName: 'sprite.css',
+            imgPath: path.src.img
+        }))
+        .pipe(gulp.dest(path.build.img));
+});
+
 gulp.task('watch', function () {
-    watch([path.watch.html], function (event, cb) {
+    watch([path.watch.html], function () {
         gulp.start('html:build');
     });
-    watch([path.watch.css], function (event, cb) {
+    watch([path.watch.css], function () {
         gulp.start('css:build');
     });
-    watch([path.watch.js], function (event, cb) {
+    watch([path.watch.js], function () {
         gulp.start('js:build');
     });
-    watch([path.watch.img], function (event, cb) {
+    watch([path.watch.img], function () {
         gulp.start('image:build');
     });
-    watch([path.watch.fonts], function (event, cb) {
-        gulp.start('fonts:build');
-    });
+});
+
+gulp.task('cacheBuster', ['html:build', 'css:build', 'js:build'], function () {
+    gulp.src(path.src.html)
+        .pipe(cache_bust())
+        .pipe(gulp.dest('.'));
 });
 
 
